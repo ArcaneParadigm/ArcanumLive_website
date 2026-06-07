4f96af62f7c98a4916616adfd30fc43377a12f07
Glenn Grillo <arcanemanifest@gmail.com>
2026-06-07 19:11:46 -0400

    Orb up 5px more (-20px -> -25px)

--- components/home2/Home2Hero.tsx
+++ components/home2/Home2Hero.tsx
@@ -121,7 +121,7 @@ export default function Home2Hero({ heroImages, uiOverlay }: Home2HeroProps) {
       {mounted && (
         <div
           className="absolute pointer-events-none"
-          style={{ left: 'calc(50vw + 4px)', top: 'calc(26vh - 20px)', transform: 'translateX(-50%)', width: '29vw', height: '29vw', zIndex: 15 }}
+          style={{ left: 'calc(50vw + 4px)', top: 'calc(26vh - 25px)', transform: 'translateX(-50%)', width: '29vw', height: '29vw', zIndex: 15 }}
         >
           <motion.div
             style={{ opacity: fade, width: '100%', height: '100%' }}
