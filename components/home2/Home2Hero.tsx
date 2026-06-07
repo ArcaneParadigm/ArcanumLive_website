33a0fbf2ff5c149b6ce17cfad3b91c4158569574
Glenn Grillo <arcanemanifest@gmail.com>
2026-06-07 19:08:05 -0400

    Orb top 18%->26% (was too high by half sphere)

--- components/home2/Home2Hero.tsx
+++ components/home2/Home2Hero.tsx
@@ -121,7 +121,7 @@ export default function Home2Hero({ heroImages, uiOverlay }: Home2HeroProps) {
       {mounted && (
         <div
           className="absolute pointer-events-none"
-          style={{ left: '50%', top: '18%', transform: 'translateX(-50%)', width: '29%', height: '29vw', zIndex: 15 }}
+          style={{ left: '50%', top: '26%', transform: 'translateX(-50%)', width: '29%', height: '29vw', zIndex: 15 }}
         >
           <motion.div
             style={{ opacity: fade, width: '100%', height: '100%' }}
